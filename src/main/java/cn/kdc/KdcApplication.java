51d6187954df719e6099a4c0bd4d78da8d40ca7e
kongdechao <1729051657@qq.com>
2018-05-15 00:43:16 +0800

    搭建完毕大概结构

---
+++ src/main/java/cn/kdc/KdcApplication.java
@@ -0,0 +1,12 @@
+package cn.kdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdcApplication.class, args);
+    }
+}
